c8f87fca96944a682c462709761b4c44f353b66e
Juha-Matti Santala <juhamattisantala@gmail.com>
2021-12-04 13:12:13 +0200

    Day 4 solved

--- src/utils.py
+++ src/utils.py
@@ -13,3 +13,12 @@ def read_input(day, transformer=str):
     except FileNotFoundError as e:
         print(e)
            
+def read_bingo_input():
+    try:
+        with open(os.path.join('..', 'inputs', 'day_4.txt')) as input_file:
+            data = input_file.read().split('\n\n')
+            numbers, boards = data[0], data[1:]
+            return numbers, boards
+    except FileNotFoundError as e:
+        print(e)
+               
