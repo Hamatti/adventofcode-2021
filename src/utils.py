ada3599088c2f2c927ecd67542fa4a13e09d613d
Juha-Matti Santala <juhamattisantala@gmail.com>
2021-12-14 12:50:01 +0200

    Refactor input reading for multisection inputs

--- src/utils.py
+++ src/utils.py
@@ -13,7 +13,25 @@ def read_input(day, transformer=str):
             return [transformer(line.strip()) for line in input_file]
     except FileNotFoundError as e:
         print(e)
-           
+        
+
+def read_multisection_input(day, transformers):
+    """
+    Given a day number (1-25), reads the corresponding input file, splits by empty line
+    and runs a transformer function from `transformers` list for each section and returns
+    a list of section outputs
+    """
+    try:
+        with open(os.path.join('..', 'inputs', f'day_{day}.txt')) as input_file:
+            output = []
+            sections = input_file.read().split('\n\n')
+            for idx, section in enumerate(sections):
+                output.append(transformers[idx](section))
+            return output
+    except FileNotFoundError as e:
+        print(e)
+
+
 def read_bingo_input():
     try:
         with open(os.path.join('..', 'inputs', 'day_4.txt')) as input_file:
@@ -21,38 +39,5 @@ def read_bingo_input():
             numbers, boards = data[0], data[1:]
             numbers = [int(n) for n in numbers.split(',')]
             return numbers, boards
-    except FileNotFoundError as e:
-        print(e)
-        
-def read_day_13():
-    try:
-        with open(os.path.join('..', 'inputs', 'day_13.txt')) as input_file:
-            coordinates_input, instructions = input_file.read().split('\n\n')
-            coordinates_input = coordinates_input.split('\n')
-            
-            coordinates = []
-            for coordinate in coordinates_input:
-                x, y = coordinate.split(',')
-                coordinates.append((int(x), int(y)))
-            folds = []
-            for instruction in instructions.split('\n'):
-                _, location = instruction.split('fold along ')
-                plane, value = location.split('=')
-                value = int(value)
-                folds.append((plane, value))
-                
-            return coordinates, folds
-    except FileNotFoundError as e:
-        print(e)
-        
-def read_day_14():
-    try:
-        with open(os.path.join('..', 'inputs', 'day_14.txt')) as input_file:
-            template, pairings = input_file.read().split('\n\n')
-            pairs = {}
-            for pairing in pairings.split('\n'):
-                start, insertion = pairing.split(' -> ')
-                pairs[start] = insertion
-            return template, pairs            
     except FileNotFoundError as e:
         print(e)
