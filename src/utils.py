ed66412616ccc6eacbaf549ee19c41037488588b
Juha-Matti Santala <juhamattisantala@gmail.com>
2021-12-14 15:19:03 +0200

    Add support to load example data

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 import os
 
 
-def read_input(day, transformer=str):
+def read_input(day, transformer=str, example=False):
     """
     Given a day number (1-25), reads the corresponding input file into
     a list with each line as an item.
@@ -9,20 +9,28 @@ def read_input(day, transformer=str):
     Runs transformer function on each item.
     """
     try:
-        with open(os.path.join('..', 'inputs', f'day_{day}.txt')) as input_file:
+        if example:
+            filename = f'day_{day}_example.txt'
+        else:
+            filename = f'day_{day}.txt'
+        with open(os.path.join('..', 'inputs', filename)) as input_file:
             return [transformer(line.strip()) for line in input_file]
     except FileNotFoundError as e:
         print(e)
         
 
-def read_multisection_input(day, transformers):
+def read_multisection_input(day, transformers, example=False):
     """
     Given a day number (1-25), reads the corresponding input file, splits by empty line
     and runs a transformer function from `transformers` list for each section and returns
     a list of section outputs
     """
     try:
-        with open(os.path.join('..', 'inputs', f'day_{day}.txt')) as input_file:
+        if example:
+            filename = f'day_{day}_example.txt'
+        else:
+            filename = f'day_{day}.txt'
+        with open(os.path.join('..', 'inputs', filename)) as input_file:
             output = []
             sections = input_file.read().split('\n\n')
             for idx, section in enumerate(sections):
@@ -32,9 +40,13 @@ def read_multisection_input(day, transformers):
         print(e)
 
 
-def read_bingo_input():
+def read_bingo_input(example=False):
     try:
-        with open(os.path.join('..', 'inputs', 'day_4.txt')) as input_file:
+        if example:
+            filename = f'day_4_example.txt'
+        else:
+            filename = f'day_4.txt'
+        with open(os.path.join('..', 'inputs', filename)) as input_file:
             data = input_file.read().split('\n\n')
             numbers, boards = data[0], data[1:]
             numbers = [int(n) for n in numbers.split(',')]
